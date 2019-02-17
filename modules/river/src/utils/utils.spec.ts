df921be4754c2993e62d5891c51781c054fb37ab
Dan Stocker <dan@kwaia.com>
2019-02-17 08:35:35 +0000

    Rearranges code in river

--- modules/river/src/utils/utils.spec.ts
+++ modules/river/src/utils/utils.spec.ts
@@ -1,4 +1,5 @@
-import {createOutPorts} from "./utils";
+import {TInPort, TOutPort} from "../types";
+import {connect, createOutPorts, disconnect, noop} from "./utils";
 
 describe("utils", () => {
   describe("createOutPorts()", () => {
@@ -7,6 +8,50 @@ describe("utils", () => {
       expect(result).toEqual({
         bar: new Set(),
         foo: new Set()
+      });
+    });
+  });
+
+  describe("connect", () => {
+    let outPort: TOutPort<number>;
+    let inPort: TInPort<number>;
+
+    beforeEach(() => {
+      outPort = new Set();
+      inPort = noop;
+    });
+
+    it("should add port to connections", () => {
+      connect(outPort, inPort);
+      expect(outPort.has(inPort)).toBeTruthy();
+    });
+  });
+
+  describe("disconnect", () => {
+    let outPort: TOutPort<number>;
+    let inPort1: TInPort<number>;
+    let inPort2: TInPort<number>;
+
+    beforeEach(() => {
+      outPort = new Set();
+      inPort1 = () => null;
+      inPort2 = () => null;
+      connect(outPort, inPort1);
+      connect(outPort, inPort2);
+    });
+
+    describe("on specifying port", () => {
+      it("should remove port from connections", () => {
+        disconnect(outPort, inPort1);
+        expect(outPort.has(inPort1)).toBeFalsy();
+        expect(outPort.has(inPort2)).toBeTruthy();
+      });
+    });
+
+    describe("on omitting port", () => {
+      it("should remove all ports from connections", () => {
+        disconnect(outPort);
+        expect(outPort.size).toBe(0);
       });
     });
   });
--- modules/river/src/utils/utils.ts
+++ modules/river/src/utils/utils.ts
@@ -1,4 +1,4 @@
-import {TOutPorts, TTag} from "../node";
+import {TInPort, TOutPort, TOutPorts, TTag} from "../types";
 
 /**
  * Creates output ports for the specified fields.
@@ -36,3 +36,28 @@ export function createOutputs<O>(outPorts: TOutPorts<O>): TOutputs<O> {
 }
 
 export const noop = () => null;
+
+/**
+ * Establishes a connection between an output port and an input port. Data
+ * emitted on an output port are automatically forwarded to connected input
+ * ports.
+ * @param outPort
+ * @param inPort
+ */
+export function connect<V>(outPort: TOutPort<V>, inPort: TInPort<V>): void {
+  outPort.add(inPort);
+}
+
+/**
+ * Tears down the connection between the specified ports, or, when the input
+ * port is omitted, tears down all downstream connections of the output port.
+ * @param outPort
+ * @param inPort
+ */
+export function disconnect<V>(outPort: TOutPort<V>, inPort?: TInPort<V>): void {
+  if (inPort) {
+    outPort.delete(inPort);
+  } else {
+    outPort.clear();
+  }
+}
