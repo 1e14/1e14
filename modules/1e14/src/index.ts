6ac0bed60d707c2c48864a5a721e9d704189e8a1
Dan Stocker <dan@kwaia.com>
2019-03-21 12:16:27 +0000

    Moves Noop to 1e14

--- modules/1e14/src/index.ts
+++ modules/1e14/src/index.ts
@@ -1,2 +1,3 @@
+export * from "./nodes";
 export * from "./types";
 export * from "./utils";
---
+++ modules/1e14/src/nodes/Noop.ts
@@ -0,0 +1,39 @@
+import {Node} from "../types";
+import {createNode} from "../utils";
+
+export type In<V> = {
+  /**
+   * Value to be forwarded.
+   */
+  d_val: V;
+};
+
+export type Out<V> = {
+  /**
+   * Forwarded input value.
+   */
+  d_val: V;
+};
+
+/**
+ * Forwards input value unconditionally.
+ * @example
+ * import {connect, createNoop} from "1e14";
+ * const noop = createNoop();
+ * connect(noop.o.d_val, console.log);
+ * noop.i.d_val(5); // logs: 5
+ */
+export type Noop<V> = Node<In<V>, Out<V>>;
+
+/**
+ * Creates a Noop node.
+ */
+export function createNoop<V>(): Noop<V> {
+  return createNode<In<V>, Out<V>>(["d_val"], (outputs) => {
+    return {
+      d_val: (value, tag) => {
+        outputs.d_val(value, tag);
+      }
+    };
+  });
+}
